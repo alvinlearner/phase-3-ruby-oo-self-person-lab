287173e0f3f73938c4ecb9fdd2bd61c66a9c1546
alvinkenyagah <alvin47k@gmail.com>
2023-05-20 17:39:40 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,91 +1,79 @@
 # your code goes here
 class Person
-    attr_accessor :balance, :happiness, :hygiene 
-    attr_reader :name
-  
-    def initialize(name, balance = 25, happiness = 8, hygiene = 8)
-      @name = name
-      @balance = balance
-      @happiness = happiness
-      @hygiene = hygiene
-      puts @name
-    end
-  
-    def bank_account(balance)
-      @balance = balance
-    end
-  
-    def happiness=(value)
-        @happiness = if value > 10
-                       10
-                     elsif value < 0
-                       0
-                     else
-                       value
-                     end
-    end
-    
-    def hygiene=(value)
-        @hygiene = if value > 10
-                     10
-                   elsif value < 0
-                     0
-                   else
-                     value
-                   end
-    end
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
 
-    def happy?
-        happiness > 7
-      end
-    
-      def clean?
-        hygiene > 7
-      end
-    
-      def get_paid(amount)
-        self.balance += amount
-        "all about the benjamins"
-      end
-    
-      def take_bath
-        self.hygiene += 4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
-      end
-    
-      def work_out
-        self.hygiene -= 3
-        self.happiness += 2
-        "♪ another one bites the dust ♫"
-      end
-    
-      def call_friend(friend)
-        self.happiness += 3
-        friend.happiness += 3
-        "Hi #{friend.name}! It's #{self.name}. How are you?"
-      end
-    
-      def start_conversation(person, topic)
-        case topic
-        when "politics"
-          [self, person].each { |p| p.happiness -= 2 }
-          "blah blah partisan blah lobbyist"
-        when "weather"
-          [self, person].each { |p| p.happiness += 1 }
-          "blah blah sun blah rain"
-        else
-          "blah blah blah blah blah"
-        end
-      end
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(num)
+    @happiness =  if num > 10
+                    10
+                  elsif num < 0
+                    0
+                  else
+                    num
+                  end
 
   end
-  
-  snoop = Person.new("Snoop", 34, -2, 15)
-  
-  
 
-puts snoop.name 
-puts snoop.balance
-puts snoop.happiness
+  def hygiene=(num)
+    @hygiene =  if num > 10
+                  10
+                elsif num < 0
+                  0
+                else
+                  num
+                end
 
 
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      [self, friend].each { |person| person.happiness -= 2 }
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      [self, friend].each { |person| person.happiness += 1 }
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+end
